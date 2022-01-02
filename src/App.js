1ea5a5642f0f82a6c884687ef4f4bb697142b68f
Sam Sycamore <samuelsycamore@gmail.com>
2022-01-01 18:22:41 -0600

    converts components to jsx

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Homeboy" animal="dog" breed="Pyrenees" />
+      <Pet name="Dingus Larry" animal="cat" breed="DSH" />
+      <Pet name="Crumbus" animal="raccoon" breed="feral af" />
+    </div>
+  );
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
